978da57cb008c05c4c18ef6bd307a2ff7e8dfbd5
Adam <adamh93@verizon.net>
2019-06-07 15:36:52 -0400

    done: Modify onSearchSbumit function to use Axios to make a get request to the unsplash API with the search term

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 
 class App extends React.Component {
 
   onSearchSubmit(term){
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID a746fd07abce4dba19f187cc5bcd41a3a5a58a97daa97b2749bf9c40555195ce'
+      }
+    }); 
   }
 
   render(){
