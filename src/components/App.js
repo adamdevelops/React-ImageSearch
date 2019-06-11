c3db4b225fe2cd9078dc38aba42a147cabd75053
Adam <adamh93@verizon.net>
2019-06-10 21:32:34 -0400

    done: Refactor Axios method to handle requests with async await, setState in onSearchSubmit function, Modify SearchBar.js onFormSubmit function to call the prop onSubmit function which takes in this.state.term

--- src/components/App.js
+++ src/components/App.js
@@ -5,19 +5,24 @@ import SearchBar from './SearchBar';
 
 class App extends React.Component {
 
-  onSearchSubmit(term){
-    axios.get('https://api.unsplash.com/search/photos', {
+  state = { images: [] };
+
+  onSearchSubmit = async term => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
       headers: {
         Authorization: 'Client-ID a746fd07abce4dba19f187cc5bcd41a3a5a58a97daa97b2749bf9c40555195ce'
       }
-    }); 
+    });
+
+    this.setState({images: response.data.results});
   }
 
   render(){
     return (
       <div className="ui container" style={{ marginTop: '10px'}}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
